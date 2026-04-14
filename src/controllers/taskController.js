dca55007ae7080701ccd5f3550890d323e3e300f
Bao Ly <bly3@charlotte.edu>
2026-04-14 14:40:25 -0400

    Assignment 10 v2

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,10 +1,5 @@
 import * as taskService from '../services/taskService.js';
 
-export async function getTasks(req, res, next) {
-  const tasks = await taskService.getAllTasks();
-  res.json(tasks);
-}
-
 export async function createTask(req, res, next) {
   const { title, completed } = req.body;
   const task = await taskService.createTask({ title, completed });
--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,8 +1,5 @@
 import * as taskRepository from '../repositories/taskRepo.js';
 
-export async function getAllTasks() {
-  return taskRepository.findAll();
-}
 
 export async function createTask(newTask) {
   return taskRepository.create(newTask);
